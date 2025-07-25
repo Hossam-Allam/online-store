b4e0bc427dff3dd41080346db06147e311b29230
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-07-25 20:44:05 +0300

    basic footer and styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,14 @@
 function Footer() {
     return (
-        <h1>Im footer</h1>
+        <div className="footer">
+            <div className="left-footer">
+                <h1>Lune</h1>
+                <p>I hope you like this fun concept project.</p>
+                <p>Lune is built using React, allowing me to stay fresh on React concepts.</p>
+                <p>If you like this project, you can check more of my work on GH</p>
+                <p>I'm open to freelancing or fulltime jobs.</p>
+            </div>
+        </div>
     )
 }
 
