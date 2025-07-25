d2e17125632351d5059c096220fa159e4874154f
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-07-25 20:29:59 +0300

    added footer to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Link } from 'react-router-dom'
 import Home from './components/Home'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <Home />
+      <Footer />
     </>
   )
 }
