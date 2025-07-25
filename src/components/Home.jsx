c5b4ca2162369f5cde424f5aa0e69edc329ff1d3
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-07-25 05:44:12 +0300

    initial structure of main home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,18 @@
+import penguin from "../assets/penguin.jpeg"
 function Home() {
 
     return (
         <>
-            <h1>Home</h1>
+            <div className="home-content">
+                <div className="mission">
+                    <h2>Our Mission</h2>
+                    <p>We believe everyone deserves to be seen, not lost in the crowd.</p>
+                    <p>At Lune, we design clothing that empowers your unique voice.</p>
+                    <p>Our pieces blend effortless edge with refined simplicity—so you stand out.</p>
+                    <p>Wear your confidence. Shine your story.</p>
+                </div>
+                <img src={penguin} alt="aura penguin" className="penguins" />
+            </div>
         </>
     )
 }
