94a4594a9a01e67f5c9dd67c22340b508e77a7a2
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-07-30 23:36:05 +0300

    default cart page if empty

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,5 +1,23 @@
 import Header from "./components/Header"
+import Footer from "./components/Footer";
+import { useCart } from "./CartContext"
+import { Link } from "react-router-dom";
 function Cart() {
+    const { cart, removeFromCart } = useCart();
+
+    if (cart.length == 0) {
+        return (
+            <div className="cart-page">
+                <Header />
+                <div className="no-items">
+                    <h1>No items in your cart</h1>
+                    <p>Head back to the <Link to={"/shop"}><i>shop</i></Link> and add items</p>
+                </div>
+                <Footer />
+            </div>
+
+        )
+    }
     return (
         <>
             <Header />
