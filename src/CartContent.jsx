c8d712a54fe09b2bce8dc1bd89fcb3e077aae03b
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-07-30 22:18:10 +0300

    move cart and functions to seperate file

---
+++ src/CartContent.jsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+export const useCart = () => useContext(CartContext);
+
+export function CartProvider({ children }) {
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = item => {
+        setCart(prev => {
+            if (prev.find(i => i.id === item.id)) return prev;
+            return [...prev, item];
+        });
+    };
+
+    const hadnleRemoveFromCart = item => {
+        setCart(prev => prev.filter(i => i.id !== item.id));
+    };
+
+    return (
+        <CartContext.Provider value={{ cart, handleAddToCart, hadnleRemoveFromCart }}>
+            {children}
+        </CartContext.Provider>
+    );
+}
