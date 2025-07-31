03c0e8d07eb19749138c07de531a37e7342b5b6e
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-07-31 05:06:25 +0300

    take in item props and display them

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -1,16 +1,25 @@
-function CartCard() {
+function CartCard({ item, RemoveFromCart }) {
+    const { images, title, price } = item;
+
     return (
         <div className="cart-card">
             <div className="cc-left">
-                <div className="cart-card-img"></div>
-                <h2>Item Name</h2>
+                <div className="cart-card-img">
+                    <img src={images[1]} alt={title} className="item-img" />
+                </div>
             </div>
+            <h2 className="cart-item-name">{title}</h2>
             <div className="cc-right">
-                <h4>34$</h4>
-                <button>Remove</button>
+                <h4 className="cart-price">{price}$</h4>
+                <button
+                    className="remove"
+                    onClick={() => RemoveFromCart(item)}
+                >
+                    Remove
+                </button>
             </div>
         </div>
-    )
+    );
 }
 
-export default CartCard
+export default CartCard;
